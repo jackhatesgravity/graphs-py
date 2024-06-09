52da5acf75a142308e35a95854b6532470227eb1
jackhatesgravity <18660090+jackhatesgravity@users.noreply.github.com>
2024-06-09 13:26:14 +0800

    Updated Graph class to use adjacency-list form

--- main.py
+++ main.py
@@ -1,84 +1,102 @@
-from typing import Union
-
-Edge = Union[str, int]  # I don't think this is right, but putting it down for now.
-
-
-class Vertex:
-    def __str__(self):
-        return f"{self.key}"
-
-    def __init__(self, key):
-        self.key = str(key)  # Enforce string type, I think?
-        self.neighbours = []  # How do I assign weights to the edges, though?
-
-
 class Graph:
-    """
-    Leaning towards making this an adjacency-list representation for a Graph.
-    Start by making something a little more basic-bitch, and then tweak as we go.
-    """
-
-    # This is uuuuuuuuuugly code. Real ugly. But it works.
-    def __str__(self):
-        result = ""
-        vertices = list(self.vertices)
-        for i in range(len(vertices) - 1):
-            result += f"{vertices[i]}, "
-        result += str(vertices[-1])
-        return result
+    node_factory = dict
+    adjlist_outer_factory = dict
+    adjlist_inner_factory = dict
+    edge_attr_factory = dict
 
     def __init__(self):
-        self.vertices = set()
+        self._node = self.node_factory()  # Captures node attributes
+        self._adj = self.adjlist_outer_factory()  # Captures outer adjacency list
 
-    def count(self):
-        return len(self.vertices)
+    def __iter__(self):
+        return iter(self._node)
 
-    def empty(self):
-        return self.count == 0
+    def __contains__(self, node):
+        try:
+            return node in self._node
+        except TypeError:
+            return False
 
-    def add_vertex(self, v):
-        if v not in self.vertices:
-            self.vertices.add(v)
-        else:
-            raise Exception("Vertex already exists in Graph!")
+    def __len__(self):
+        return len(self._node)
 
-    # Removing a vertex needs to take all of its references with it.
-    def remove_vertex(self, v):
-        if v in self.vertices:
-            self.vertices.remove(v)
-        else:
-            raise Exception("Vertex not found in Graph!")
+    def add_node(self, node_to_add):
+        # Null check
+        if node_to_add is None:
+            raise ValueError("Node cannot be none!")
 
-    # Also, I like the CLRS description of a weight function. Keep it separate.
+        # Check if the node already exists
+        if node_to_add in self._node.keys():
+            raise Exception("Node already in graph!")
+
+        self._adj[node_to_add] = self.adjlist_inner_factory()
+        self._node[node_to_add] = self.node_factory()
+
+    def add_edge(self, u, v, **attr):
+        # Add the nodes
+        if u not in self._node:
+            if u is None:
+                raise ValueError("Node cannot be none!")
+            self._adj[u] = self.adjlist_inner_factory()
+            self._node[u] = self.node_factory()
+        if v not in self._node:
+            if v is None:
+                raise ValueError("Node cannot be none!")
+            self._adj[v] = self.adjlist_inner_factory()
+            self._node[v] = self.node_attr_factory()
+
+        # Add the edge
+        datadict = self._adj[u].get(v, self.edge_attr_factory())
+        datadict.update(attr)  # Allows us to add whatever attributes we want, including weights.
+        self._adj[u][v] = datadict
+        self._adj[v][u] = datadict
+
+    def remove_node(self, node_to_remove):
+        # Null check
+        if node_to_remove is None:
+            raise ValueError("Node cannot be none!")
+
+        # Check if the node already exists
+        if node_to_remove not in self._node:
+            raise Exception("Node not in graph!")
+
+        # Remove node from the list of nodes
+        adj = self._adj
+        neighbours = list(adj[node_to_remove])
+        del self._node[node_to_remove]
+
+        # Remove node from adjacency lists
+        for neighbour in neighbours:
+            del adj[neighbour][node_to_remove]
+        del adj[node_to_remove]
+
+    def nodes(self):
+        return self._node.items()
+
+    def edges(self):
+        return self._adj.items()
 
 
 def main():
-    g = Graph()
-    v1 = Vertex("v1")
-    v2 = Vertex("v2")
-    v3 = Vertex("v3")
+    G = Graph()
+    [G.add_node(x) for x in range(5)]
+    print(G.nodes())
+    print(G.edges())
+    print()
 
-    verts = [v1, v2, v3]
-    for v in verts:
-        g.add_vertex(v)
-    print(g)
+    G.add_edge(0, 1, weight=11)
+    G.add_edge(1, 2, weight=7)
+    G.add_edge(0,2,weight=3)
+    print(G.nodes())
+    print(G.edges())
+    print()
 
-    try:
-        g.add_vertex(v1)
-    except Exception as e:
-        print(e)
-
-    g.remove_vertex(v1)
-    print(g)
-
-    try:
-        g.remove_vertex(v1)
-    except Exception as e:
-        print(e)
-
-    g.add_vertex(v1)
-    print(g)
+    G.remove_node(1)
+    print(G.nodes())
+    print(G.edges())
 
 
 if __name__ == "__main__":
     main()
+
+
